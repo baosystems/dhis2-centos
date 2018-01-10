555246aeb2fde0a41c3d0dc7d7fb780c9e48dbf5
Alan Ivey <alanivey@gmail.com>
2018-01-10 10:21:11 -0500

    Use Ansible for more commands

--- dhis2-centos.sh
+++ dhis2-centos.sh
@@ -8,6 +8,7 @@ yum -y -d1 -e0 install -- \
   ansible \
   libselinux-python libsemanage-python \
   python-psycopg2 \
+  unzip
 
 
 # Install main applications: Java, Nginx, Tomcat, PostgreSQL Server
@@ -22,8 +23,8 @@ systemctl enable nginx
 systemctl enable tomcat
 systemctl enable postgresql
 
-postgresql-setup initdb
 # PostgreSQL: initialize database and setup for local workstation use
+ansible all -i 'localhost,' -c local -m command -a "postgresql-setup initdb creates=/var/lib/pgsql/data/postgresql.conf"
 ansible all -i 'localhost,' -c local -m replace -a "path=/var/lib/pgsql/data/pg_hba.conf regexp='^(local\s+.*\s+peer$)' replace='\1 map=local_users'"
 ansible all -i 'localhost,' -c local -m replace -a "path=/var/lib/pgsql/data/pg_hba.conf regexp='^(host\s+all\s+all\s+.*\s+)ident$' replace='\1 md5'"
 ansible all -i 'localhost,' -c local -m lineinfile -a "path=/var/lib/pgsql/data/pg_ident.conf line='local_users  postgres  postgres'"
@@ -33,8 +34,8 @@ systemctl start postgresql && sleep 5
 ansible all -i 'localhost,' -c local -m postgresql_user -a "name=dhis encrypted=yes password=\"{{ lookup('password', '/root/pg_dhis_passwd.txt chars=ascii_letters,digits') }}\""
 ansible all -i 'localhost,' -c local -m postgresql_db -a "name=dhis2 owner=dhis"
 
-install -o tomcat -g tomcat -d /opt/dhis2/
 # DHIS2 configuration file for connecting to the database
+ansible all -i 'localhost,' -c local -m file -a "path=/opt/dhis2 state=directory owner=tomcat group=tomcat"
 ansible all -i 'localhost,' -c local -m copy -a "dest=/opt/dhis2/dhis.conf owner=tomcat group=tomcat backup=yes content='connection.dialect = org.hibernate.dialect.PostgreSQLDialect
 connection.driver_class = org.postgresql.Driver
 connection.url = jdbc:postgresql:dhis2
@@ -43,15 +44,18 @@ connection.password = {{ lookup('password', '/root/pg_dhis_passwd.txt chars=asci
 connection.schema = update
 '"
 
-ansible all -i 'localhost,' -c local -m lineinfile -a "path=/etc/sysconfig/tomcat line='JAVA_OPTS=\"-Djava.security.egd=file:/dev/./urandom\"'"
 # Configure Tomcat for '/dev/urandom' -- see https://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source
+ansible all -i 'localhost,' -c local -m lineinfile -a "path=/etc/sysconfig/tomcat line='JAVA_OPTS=\"-Djava.security.egd=file:/dev/./urandom\"'"
 ansible all -i 'localhost,' -c local -m lineinfile -a "path=/etc/tomcat/tomcat.conf line='JAVA_OPTS=\"-Djava.security.egd=file:/dev/./urandom\"'"
+
 # Download and extract DHIS 2.28, start Tomcat
-su - tomcat -s /bin/bash -c "curl -L -o /var/lib/tomcat/webapps/ROOT.war https://www.dhis2.org/download/releases/2.28/dhis.war"
+ansible all -i 'localhost,' -c local -m get_url -a "url=https://s3-eu-west-1.amazonaws.com/releases.dhis2.org/2.28/dhis.war dest=/var/lib/tomcat/webapps/ROOT.war owner=tomcat group=tomcat"
+ansible all -i 'localhost,' -c local -m file -a "path=/var/lib/tomcat/webapps/ROOT state=directory owner=tomcat group=tomcat"
+ansible all -i 'localhost,' -c local -m unarchive -a "src=/var/lib/tomcat/webapps/ROOT.war dest=/var/lib/tomcat/webapps/ROOT/ owner=tomcat group=tomcat"
 systemctl start tomcat
 
-ansible all -i 'localhost,' -c local -m lineinfile -a "path='/etc/nginx/nginx.conf' insertafter='^[^#]\s+location\s+/\s+{' line='          proxy_pass  http://localhost:8080;' validate='nginx -c %s -t'"
 # Configure and start Nginx
+ansible all -i 'localhost,' -c local -m lineinfile -a "path='/etc/nginx/nginx.conf' insertafter='^[^#]\s+location\s+/\s+{' line='          proxy_pass  http://localhost:8080;' validate='nginx -c %s -t'"
 ansible all -i 'localhost,' -c local -m lineinfile -a "path='/etc/nginx/nginx.conf' insertafter='^[^#]\s+proxy_pass\s+' line='          proxy_set_header  Host               \$http_host;' validate='nginx -c %s -t'"
 ansible all -i 'localhost,' -c local -m lineinfile -a "path='/etc/nginx/nginx.conf' insertafter='^[^#]\s+proxy_set_header\s+Host\s+' line='          proxy_set_header  X-Forwarded-Proto  \$scheme;' validate='nginx -c %s -t'"
 systemctl start nginx
